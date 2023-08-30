59a4ed3532a9fb6424953c8d90f02c5dbedc28f9
malit mohamut <60866464+mawlid1431@users.noreply.github.com>
2023-08-30 16:56:37 +0300

    THe first names of each array

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-// Good Luck 💪🏾
+function getFirstNames(namesArray) {
+  const firstNames = namesArray.map(nameGroup => nameGroup[0]);
+  return firstNames;
+}
+
+const names = [["Ahmed","Aisha","Amina"],["Jaamac","Khadijah","Layla"],["Mohamed","Ali"]];
+const firstNames = getFirstNames(names);
+console.log(firstNames); // Output: ["Ahmed","Jaamac","Mohamed"]
+
